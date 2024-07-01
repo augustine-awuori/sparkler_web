4c89e48fa85465b6a2bc0bc39a5ab1d27a7769c2
augustine-awuori <codewithaugustine@gmail.com>
2024-07-02 01:06:30 +0300

    Create the 'Create Sparkle Dialog' Component

---
+++ src/components/sparkle/CreateSparkleDialog.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+import Modal from "../Modal";
+import SparkleForm from "./SparkleForm";
+
+const Container = styled.div`
+  .modal-block {
+    margin-top: 20px;
+    padding: 15px;
+    width: 600px;
+    height: max-content;
+    z-index: 10;
+  }
+
+  .tweet-form {
+    margin-top: 20px;
+  }
+`;
+
+interface Props {
+  onClickOutside: () => void;
+}
+
+export default function CreateTweetDialog({ onClickOutside }: Props) {
+  const onSubmit = async (_text: string) => {
+    // create sparkle
+
+    onClickOutside();
+  };
+
+  return (
+    <Container>
+      <Modal onClickOutside={onClickOutside} className="modal-block">
+        <SparkleForm
+          onSubmit={onSubmit}
+          shouldFocus={true}
+          minHeight={240}
+          className="tweet-form"
+          placeholder="What's happening"
+        />
+      </Modal>
+    </Container>
+  );
+}
