6a7a7acbea47859e840a491bd3d5ee87518fce04
augustine-awuori <codewithaugustine@gmail.com>
2024-07-24 18:32:29 +0300

    Improve layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,49 +1,19 @@
 import { ReactNode, useState } from "react";
 import { useStreamContext } from "react-activity-feed";
 import styled from "styled-components";
+import { Box, useBreakpointValue } from "@chakra-ui/react";
 
 import LeftSide from "./LeftSide";
 import CreateTweetDialog from "./sparkle/CreateSparkleDialog";
 import RightSide from "./RightSide";
 import LoadingIndicator from "./LoadingIndicator";
 
-const Container = styled.div`
-  min-height: 100vh;
-  background: black;
-  --left: 240px;
-  --right: 300px;
-  --middle: calc(100% - var(--left) - var(--right));
-
-  .content {
-    max-width: 1300px;
-    margin: 0 auto;
-    width: 100%;
-    display: flex;
-  }
-
-  .left-side-bar {
-    height: 100vh;
-    width: var(--left);
-    position: sticky;
-    top: 0;
-  }
-
-  .main-content {
-    position: relative;
-    width: var(--middle);
-    border-left: 1px solid #333;
-    border-right: 1px solid #333;
-    min-height: 100vh;
-  }
-
-  .right-side-bar {
-    width: var(--right);
-  }
-`;
-
 export default function Layout({ children }: { children: ReactNode }) {
   const { user } = useStreamContext();
   const [createDialogOpened, setCreateDialogOpened] = useState(false);
+
+  const showLeftSidebar = useBreakpointValue({ base: false, md: true });
+  const showRightSidebar = useBreakpointValue({ base: false, lg: true });
 
   if (!user) return <LoadingIndicator />;
 
@@ -56,17 +26,52 @@ export default function Layout({ children }: { children: ReactNode }) {
       )}
       <Container>
         <div className="content">
-          <div className="left-side-bar">
-            <LeftSide onClickSparkle={() => setCreateDialogOpened(true)} />
-          </div>
+          {showLeftSidebar && (
+            <Box className="left-side-bar">
+              <LeftSide onClickSparkle={() => setCreateDialogOpened(true)} />
+            </Box>
+          )}
           <main className="main-content">
             {!user ? <LoadingIndicator /> : children}
           </main>
-          <div className="right-side-bar">
-            <RightSide />
-          </div>
+          {showRightSidebar && (
+            <Box className="right-side-bar">
+              <RightSide />
+            </Box>
+          )}
         </div>
       </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  min-height: 100vh;
+  background: black;
+
+  .content {
+    max-width: 1300px;
+    margin: 0 auto;
+    width: 100%;
+    display: flex;
+  }
+
+  .left-side-bar {
+    height: 100vh;
+    width: 240px;
+    position: sticky;
+    top: 0;
+  }
+
+  .main-content {
+    position: relative;
+    width: 100%;
+    border-left: 1px solid #333;
+    border-right: 1px solid #333;
+    min-height: 100vh;
+  }
+
+  .right-side-bar {
+    width: 300px;
+  }
+`;
