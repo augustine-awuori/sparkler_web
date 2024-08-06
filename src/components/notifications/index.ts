ee80cc57af8d7f5745f5213deeeebc02183e75bc
augustine-awuori <codewithaugustine@gmail.com>
2024-08-07 00:08:38 +0300

    Code refactor

--- src/components/notifications/index.ts
+++ src/components/notifications/index.ts
@@ -1,5 +1,4 @@
 export { default as CommentNotification } from "./CommentNotification";
 export { default as FollowNotification } from "./FollowNotification";
 export { default as LikeNotification } from "./LikeNotification";
-export { default as NotificationContent } from "./NotificationContent";
 export { default as NotificationGroup } from "./NotificationGroup";
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,59 @@
-import HomeContent from "../components/home/HomeContent";
+import styled from "styled-components";
+import { Feed } from "react-activity-feed";
 
-export default function Home() {
-  return <HomeContent />;
+import { useUser } from "../hooks";
+import LoadingIndicator from "../components/LoadingIndicator";
+import MainHeader from "../components/home/MainHeader";
+import CreateSparkleTop from "../components/home/CreateSparkleTop";
+import Timeline from "../components/home/Timeline";
+
+export default function HomePage() {
+  const { user } = useUser();
+
+  if (!user)
+    return (
+      <Container>
+        <LoadingIndicator />
+      </Container>
+    );
+
+  return (
+    <Container>
+      <div className="header">
+        <MainHeader />
+      </div>
+      <Feed feedGroup="user">
+        <div className="create-tweet-top">
+          <CreateSparkleTop />
+        </div>
+        <Timeline />
+      </Feed>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  .header {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+  }
+
+  .create-tweet-top {
+    border-bottom: 1px solid #333;
+  }
+
+  .new-tweets-info {
+    border-bottom: 1px solid #333;
+    padding: 20px;
+    text-align: center;
+    color: var(--theme-color);
+    display: block;
+    width: 100%;
+    font-size: 16px;
+
+    &:hover {
+      background: #111;
+    }
+  }
+`;
