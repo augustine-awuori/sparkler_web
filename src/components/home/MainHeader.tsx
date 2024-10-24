6fad33eaee295871f71e60d2d3efb9e6b36d11d9
augustine-awuori <codewithaugustine@gmail.com>
2024-10-24 10:38:30 +0300

    Show clearly the login button for mobile devices

--- src/components/home/MainHeader.tsx
+++ src/components/home/MainHeader.tsx
@@ -1,12 +1,40 @@
 import styled from "styled-components";
+import { useBreakpointValue } from "@chakra-ui/media-query";
+import { FaSignInAlt } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
+import { Button } from "@chakra-ui/react";
+import { useUser } from "../../hooks";
 import Sparkler from "../icons/Twitter";
 
 export default function MainHeader() {
+  const { user } = useUser();
+  const isMobile = useBreakpointValue({ base: true, md: false });
+  const navigate = useNavigate();
+
   return (
     <Header>
       <h1>Home</h1>
-      <Sparkler color="white" />
+      {user ? (
+        <Sparkler color="white" />
+      ) : (
+        <>
+          {isMobile ? (
+            <Button
+              size="sm"
+              color="var(--conc-theme-color)"
+              letterSpacing={0.1}
+              colorScheme="var(--conc-theme-color)"
+              onClick={() => navigate("/auth")}
+              leftIcon={<FaSignInAlt />}
+            >
+              Login
+            </Button>
+          ) : (
+            <Sparkler color="white" />
+          )}
+        </>
+      )}
     </Header>
   );
 }
