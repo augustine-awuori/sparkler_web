9e0640d1ebd589d9aba36655a3afcbde33fdefc2
augustine-awuori <codewithaugustine@gmail.com>
2024-11-06 10:58:56 +0300

    Bug fix: view sparkle details

--- src/components/sparkle/SparkleBlock.tsx
+++ src/components/sparkle/SparkleBlock.tsx
@@ -273,7 +273,7 @@ const SparkleBlock: React.FC<Props> = ({ activity }) => {
     return null;
 
   return (
-    <Box _hover={{ bg: "#111" }}>
+    <Box _hover={{ bg: "#111" }} onClick={viewDetails}>
       <Block>
         {(isAReaction || hasResparkled) && (
           <Flex align="center" mb={1.5} color="#777" fontSize="small" ml={10}>
